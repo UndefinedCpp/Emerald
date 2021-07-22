0c3ce243c6cb0e1ae4736b8b2ff500454a9d4481
UndefinedCpp <39729414+UndefinedCpp@users.noreply.github.com>
2021-07-22 17:35:51 +0800

    Delete build.cmd

--- src/lexer/build.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Start building... g++ lexer.test.cpp lexer.cpp -o lexer.test.exe
-D:\Dev-Cpp\MinGW64\bin\g++ lexer.test.cpp lexer.cpp -o lexer.test.exe -std=gnu++11 -w -I"D:\Dev-Cpp\MinGW64\include" -I"D:\Dev-Cpp\MinGW64\x86_64-w64-mingw32\include" -I"D:\Dev-Cpp\MinGW64\lib\gcc\x86_64-w64-mingw32\4.9.2\include" -I"D:\Dev-Cpp\MinGW64\lib\gcc\x86_64-w64-mingw32\4.9.2\include\c++" -L"D:\Dev-Cpp\MinGW64\lib" -L"D:\Dev-Cpp\MinGW64\x86_64-w64-mingw32\lib" -O3
-echo Build finish!
-pause
-echo Running...
-lexer.test.exe
-pause
