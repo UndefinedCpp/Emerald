274f9665702da1eb8e60190c9fd231f040804d9c
UndefinedCpp <39729414+UndefinedCpp@users.noreply.github.com>
2021-07-22 18:21:14 +0800

    Update lexer.test.cpp

--- src/lexer/lexer.test.cpp
+++ src/lexer/lexer.test.cpp
@@ -2,33 +2,47 @@
 #include <string>
 #include <cstring>
 #include "lexer.h"
+
 using namespace std;
 LexerMsp::Lexer* lex;
 int main()
 {
-	char input[128];
+	char input[256];
 	LexerMsp::Token tk;
 	memset(input, 0, sizeof(input));
-	
-	cout << "Type\t\tValue\t\tErrorCode" << endl;
-	
+	int cnt = 0;
+	// QueryPerformanceFrequency(&tc);
+
+
 	while(true)
 	{
+		cnt = 0;
 		cout << "> ";
-		cin.getline(input, 128);
+		cin.getline(input, 256);
 		
 		lex = new LexerMsp::Lexer(input);
 		while(true)
 		{
+			// QueryPerformanceCounter(&t1);
 			tk = lex->read();
-			if(tk.type == LexerMsp::EMPTY)
+			// QueryPerformanceCounter(&t2);
+			// cout << "TimeConsume:" << ((t2.QuadPart - t1.QuadPart)*1.0 / tc.QuadPart); 
+			
+			if(tk.errcode != 0xff)
 			{
-				cout << "* error: " << tk.value << endl;
+				cout << "[error]" << tk.value << endl;
+				break;
+			}
+			else if(tk.type == LexerMsp::EMPTY && tk.errcode == 0xff)
+			{
+				cout << "(DONE)" << endl;
 				break;
 			}
 			else
 			{
-				cout << tk.type << "\t\t" << tk.value << "\t\t" << tk.errcode << endl;
+				cout << "(" << tk.type << ": " << tk.value << ") ";
+				if(++cnt % 10 == 0)
+					cout << endl;
 			}
 		}
 		delete lex;
